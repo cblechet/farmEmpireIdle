e5315064dac2896e57da2127c4db55f4505588d5
cblechet <webmaster@colinblechet.fr>
2024-03-23 00:58:10 +0100

    save config angular json

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../auth.service'; // Assure-toi que le chemin est c
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'] // Utilise styleUrls au lieu de styleUrl
+  styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
   email: string = '';
